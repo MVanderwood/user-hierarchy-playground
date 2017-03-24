10cb443008e7c037793116bb37cf22299ca15739
MVanderwood <m.b.vanderwood@gmail.com>
2017-03-23 22:51:21 -0500

    Initial commit & configurations.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  self.abstract_class = true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
